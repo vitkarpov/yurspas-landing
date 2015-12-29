e412e942e1044fb12ca913c70f6bf68ab5543f00
vitkarpov <viktor.s.karpov@gmail.com>
2015-12-29 15:13:38 +0300

    description

--- common.blocks/root/root.bemtree.js
+++ common.blocks/root/root.bemtree.js
@@ -6,7 +6,7 @@ block('root').replace()(function() {
         title: this.data.title,
         favicon : '/favicon.ico',
         head : [
-            { elem : 'meta', attrs : { name : 'description', content : '' } },
+            { elem : 'meta', attrs : { name : 'description', content : this.data.description } },
             { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
             { elem : 'css', url : 'index.min.css' },
             { elem : 'css', url : 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&subset=cyrillic' }
