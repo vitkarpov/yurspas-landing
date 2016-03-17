c2b378c5930978fd4284367e634e5ddf5469927c
vitkarpov <viktor.s.karpov@gmail.com>
2016-03-17 12:12:58 +0300

    there's no need to use forms.php

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,6 @@ module.exports = function (grunt) {
                             "*.min.js",
                             "images/**"
                         ]
-                    },
-                    {
-                        expand: true,
-                        src: "forms.php"
                     }
                 ]
             }
